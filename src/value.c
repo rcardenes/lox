ad521c11db91d9cdd50c86167a3f763d12360b7c
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2022-11-23 09:36:04 -0300

    Optimize the size of Value using NaN boxing

--- src/value.c
+++ src/value.c
@@ -29,6 +29,27 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+	if (IS_BOOL(value)) {
+		printf(AS_BOOL(value) ? "true" : "false");
+	}
+	else if (IS_NIL(value)) {
+		printf("nil");
+	}
+	else if (IS_NUMBER(value)) {
+		double d = AS_NUMBER(value);
+		int i = (int)d;
+		if (d == i) {
+			printf("%d", i);
+		}
+		else {
+			printf("%g", d);
+		}
+	}
+	else if (IS_OBJ(value)) {
+		printObject(value);
+	}
+#else
 	switch (value.type) {
 		case VAL_BOOL: printf(AS_BOOL(value) ? "true" : "false"); break;
 		case VAL_NIL: printf("nil"); break;
@@ -45,9 +66,17 @@ void printValue(Value value) {
 		}
 		case VAL_OBJ: printObject(value); break;
 	}
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+	if (IS_NUMBER(a) && IS_NUMBER(b)) {
+		return AS_NUMBER(a) == AS_NUMBER(b);
+	}
+	else
+		return a == b;
+#else
 	if (a.type != b.type) return false;
 	switch (a.type) {
 		case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
@@ -56,4 +85,5 @@ bool valuesEqual(Value a, Value b) {
 		case VAL_OBJ: return AS_OBJ(a) == AS_OBJ(a);
 		default: return false; // Unreachable
 	}
+#endif
 }
